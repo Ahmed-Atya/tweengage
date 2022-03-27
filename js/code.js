7e038e169588f8546de51731b8c8fc04128fdb68
Ahmed-Atya <ahmedatya@outlook.com>
2022-03-27 19:32:13 +0200

    main files of the template

---
+++ js/code.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+//<!-- Initialize the plugin -->
+//<!-- Initialize with options, if needed -->
+    $('#exampleSlider').multislider({
+        interval:4000,
+        slideAll:true
+    });  
+    //Adding First Class to all Sections
+    
+	      
+});
+
